94ac7ee9633a1864b82fba6a87eee334672b68c1
Harsh Patel <harshppatel2880@gmail.com>
2020-05-03 21:22:06 -0500

    About page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserIconComponent } from './browser-icon/browser-icon.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BrowserIconComponent } from './browser-icon/browser-icon.component';
     HeaderComponent,
     HomeComponent,
     BrowserIconComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
--- todo.md
+++ todo.md
@@ -1,1 +1,12 @@
-[ ] write tests for browser lib files
+### ToDos
+
+[x] Home Page
+[x] About Page
+[ ] 404 Page
+[ ] add favicon
+[ ] make it responsive
+[ ] write tests
+
+### strecthed
+[ ] improve a11y
+[ ] add animation
